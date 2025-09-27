df747c64ffff0721d934b3465eccf1f55aab03d5
W-Kuria <william.njoroge.kuria@gmail.com>
2025-09-27 23:12:24 +0300

    feat: Complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,30 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route("/messages", methods=["GET"])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route("/messages", methods=["POST"])
+def create_message():
+    data = request.get_json()
+    new_msg = Message(body=data["body"], username=data["username"])
+    db.session.add(new_msg)
+    db.session.commit()
+    return jsonify(new_msg.to_dict()), 201
 
-if __name__ == '__main__':
-    app.run(port=5555)
+@app.route("/messages/<int:id>", methods=["PATCH"])
+def update_message(id):
+    msg = Message.query.get_or_404(id)
+    data = request.get_json()
+    msg.body = data.get("body", msg.body)
+    db.session.commit()
+    return jsonify(msg.to_dict())
+
+@app.route("/messages/<int:id>", methods=["DELETE"])
+def delete_message(id):
+    msg = Message.query.get_or_404(id)
+    db.session.delete(msg)
+    db.session.commit()
+    return "", 204
